185d6e035cd1379bee2fdfae1eb2fd90f4308290
Mehedi Hasan <jibonmh7@gmail.com>
2021-05-03 21:44:01 +0600

    added UI elements

--- src/vlc/MainPageController.java
+++ src/vlc/MainPageController.java
@@ -2,24 +2,40 @@ package vlc;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class MainPageController {
+public class MainPageController implements Initializable {
     public AnchorPane mainAnchorPane;
     public static int videoCnt=0;
     public static double totalSecCnt=0;
     public CheckBox isSubDirIncluded;
+    public VBox mainPageVBox;
+    public ProgressBar mainProgressBar;
+    public HBox videoCntHbox;
+    public HBox durationHBox;
+    public Label helloLabel;
+    public Button selectDirectoryButton;
+    public Label videoDurationLabel;
+    public Label videoCntLabel;
 
 
     public void selectDirectoryButtonAction(ActionEvent actionEvent) throws IOException, InterruptedException {
@@ -29,21 +45,65 @@ public class MainPageController {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         Stage primaryStage=(Stage) mainAnchorPane.getScene().getWindow();
         File selectedDirectory = directoryChooser.showDialog(primaryStage);
-        Parent waitingRoot= FXMLLoader.load(getClass().getResource("WaitingPage.fxml"));
-       // WaitingPageController.progressBarInitialize();
-        primaryStage.setScene(new Scene(waitingRoot));
+
+        modifyUI();
+
         try {
+            //modifyUI();
             doVideoCalculation(selectedDirectory);
         } catch (Exception e){
-           // e.printStackTrace();
+            e.printStackTrace();
 
         } finally{
+
             System.out.println("total videos "+videoCnt);
             System.out.println("total time in secs "+totalSecCnt);
+            String s=Integer.toString(videoCnt);
+            videoCntLabel.setText(s);
+            s=Double.toString(totalSecCnt);
+
+            String formattedString="";
+            String hourVal,minVal,secVal;
+
+            hourVal=Integer.toString((int)totalSecCnt/(60*60));
+
+            minVal=Integer.toString((int)(totalSecCnt%3600)/60);
+
+            secVal=Integer.toString((int)totalSecCnt%60);
+
+            if(!hourVal.equals("0")){
+                formattedString+=hourVal;
+                formattedString+="hour : ";
+            }
+            if(!minVal.equals("0")){
+                formattedString+=minVal;
+                formattedString+=" minute : ";
+            }
+            if(!secVal.equals("0")){
+                formattedString+=secVal;
+                formattedString+=" second : ";
+            }
+            int len=formattedString.length();
+            if(formattedString.endsWith(" ")) formattedString=formattedString.substring(0,len-2);
+            System.out.println(formattedString+"@");
+            videoDurationLabel.setText(formattedString);
+
+            if(videoCnt==0){
+                videoDurationLabel.setText("No video files found");
+            }
         }
 
+    }
+
+    public void modifyUI() throws IOException{
 
 
+        videoCntHbox.setVisible(true);
+        durationHBox.setVisible(true);
+
+
+
+        mainPageVBox.getChildren().remove(0,3);
     }
 
 
@@ -75,4 +135,10 @@ public class MainPageController {
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        videoCntHbox.setVisible(false);
+        durationHBox.setVisible(false);
+
+    }
 }
---
+++ src/vlc/ResultsPageController.java
@@ -0,0 +1,10 @@
+package vlc;
+
+import javafx.scene.control.Label;
+
+public class ResultsPageController {
+    public Label videoCountLabel;
+    public Label videoDurationLabel;
+
+
+}
